82ee46a8aeb2eded1c318360e46606680082b1fb
Suruthi Raman <suruthiram19@gmail.com>
2021-10-22 16:35:46 +0200

    Learning purpose

---
+++ JavaMethods/src/JavaMethodOverloading.java
@@ -0,0 +1,19 @@
+ // same name with different parameter
+public class JavaMethodOverloading {
+	
+	static int plusMethodInt(int x, int y) {
+		return x+y ;
+	}
+    
+	static double plusMethodDouble (double x, double y) {
+		return x + y;
+		
+	}
+	
+	public static void main (String[] args) {
+		int myNum1 = plusMethodInt(8,5);
+		double myNum2 = plusMethodDouble(3.3,2.5);
+		System.out.println("int:" + myNum1);
+		System.out.println("double:" + myNum2);
+	}
+}
